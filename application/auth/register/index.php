6653722c74a6020674994abf4b06f58fc993c99c
Amos <amos7perez@gmail.com>
2025-09-08 20:50:47 -0400

    Changed AJAX API from XHR to Fetch

--- application/auth/register/index.php
+++ application/auth/register/index.php
@@ -14,38 +14,34 @@
                         <p class="text-center text-sm text-gray-500">Enter your details below to create your account</p>
                     </div>
                 </div>
-                <form id="registration-form" class="flex flex-col gap-6">
+                <form id="registration-form" class="flex flex-col gap-2">
+
                     <div class="grid gap-2">
-                        <label class="text-sm font-medium" for="first_name">First name</label>
-                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-1 shadow-md" placeholder="First name" type="text" id="first_name" name="first_name" required >
-                    
-                        </input>
+                        <label for="first_name" class="text-sm font-medium">First name</label>
+                        <input id="first_name" class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-2 shadow-md" placeholder="First name" type="text" name="first_name" required />
+                        <span class="text-right text-xs text-red-600 min-h-[1rem]" id="first_name_error"></span>
                     </div>
                     <div class="grid gap-2">
-                        <label class="text-sm font-medium" for="last_name">Last name</label>
-                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-1 shadow-md" placeholder="Last name" type="text" id="last_name" name="last_name" required >
-                    
-                        </input>
+                        <label for="last_name" class="text-sm font-medium">Last name</label>
+                        <input id="last_name" class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-2 shadow-md" placeholder="Last name" type="text" name="last_name" required />
+                        <span class="text-right text-xs text-red-600 min-h-[1rem]" id="last_name_error"></span>
                     </div>
                     <div class="grid gap-2">
-                        <label class="text-sm font-medium" for="email">Email Address</label>
-                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-1 shadow-md" placeholder="email@email.com" type="email" id="email" name="email" required >
-                    
-                        </input>
+                        <label for="email" class="text-sm font-medium">Email Address</label>
+                        <input id="email" class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-2 shadow-md" placeholder="email@example.com" type="email" name="email" required />
+                        <span class="text-right text-xs text-red-600 min-h-[1rem]" id="email_error"></span>
                     </div>
                     <div class="grid gap-2">
-                        <label class="text-sm font-medium" for="password">Password</label>
-                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-1 shadow-md" placeholder="Password" type="password" id="password" name="password" required >
-                    
-                        </input>
+                        <label for="password" class="text-sm font-medium">Password</label>
+                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-2 shadow-md" placeholder="Password" type="password" id="password" name="password" required />
+                        <span class="text-right text-xs text-red-600 min-h-[1rem]" id="password_error"></span>
                     </div>
                     <div class="grid gap-2">
-                        <label class="text-sm font-medium" for="confirm-password">Confirm password</label>
-                        <input class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-1 shadow-md" placeholder="Confirm password" type="password" id="confirm-password" name="confirm-password" required >
-                    
-                        </input>
+                        <label for="confirm_password" class="text-sm font-medium">Confirm password</label>
+                        <input id="confirm_password" class="border rounded-md border-gray-300 placeholder:text-gray-400 px-3 py-2 shadow-md" placeholder="Confirm password" type="password" name="confirm_password" required >
+                        <span class="text-right text-xs text-red-600 min-h-[1rem]" id="confirm_password_error"></span>
                     </div>
-                    <button type="button" onclick="register(event)" class="flex items-center justify-center gap-2 bg-gray-800 text-white px-4 py-2 rounded-md mt-4 text-xs cursor-pointer">
+                    <button id="create_account" type="button" class="flex items-center justify-center gap-2 bg-gray-800 text-white px-4 py-3 rounded-md mt-4 text-sm cursor-pointer">
                         Create account
                     </button>
                     <div class="text-gray-400 text-center text-sm">
@@ -57,19 +53,70 @@
     </div>
 
     <script>
-        function register(event) {
-            console.log("register() called");
+        document.getElementById("create_account").addEventListener("click", async (event) => 
+        {
             event.preventDefault();
-            const form = document.getElementById("registration-form");
-            const formData = new FormData(form);
-            const formObject = Object.fromEntries(formData.entries());
-            const formJSON = JSON.stringify(formObject)
-            const xhr = new XMLHttpRequest();
 
-            xhr.open("POST", "/api/auth/register/", true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.send(formJSON);
-        }
+            const formData = new FormData(document.getElementById("registration-form"));
+            const data = Object.fromEntries(formData.entries());
+            data.first_name = data.first_name.trim();
+            data.last_name = data.last_name.trim();
+            data.email = data.first_name.trim().toLowerCase();
+
+            if (!data.first_name) 
+                document.getElementById("first_name_error").textContent = "Required";
+
+            if (!data.last_name) 
+                document.getElementById("last_name_error").textContent = "Required";
+
+            if (!data.email) 
+                document.getElementById("email_error").textContent = "Required";
+
+            if (data.password !== data.confirm_password) 
+                document.getElementById("confirm_password_error").textContent = "Password mismatch";
+
+            if (!data.last_name || !data.email || data.password !== data.confirm_password) 
+                return;
+
+
+            body = JSON.stringify(data);
+
+            try 
+            {
+                const response = await fetch("/api/auth/register/", 
+                {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: body
+                });
+
+                if (response.ok) // If (response.status >= 200) && (response.status <= 299)
+                {
+                    const data = await response.json();
+                    /* 
+                    data = {
+                        "message": "Success! User registered.",
+                        "user": {
+                            "id": integer,
+                            "first_name": string,
+                            "last_name": string, 
+                        }
+                    }*/
+                    window.location.href = "/contacts/"
+                }
+                else // If (response.status < 200) || (response.status > 299)
+                {
+                    const error = await response.json();
+                    error.status = response.status;
+                    error.statusText = response.statusText;
+                    throw error;
+                }
+            } 
+            catch (error)
+            {
+                // Error handling coming soon!
+            }
+        });
     </script>
 
 <?php require_once COMPONENTS . '/layout/layout-bottom.php' ?>
