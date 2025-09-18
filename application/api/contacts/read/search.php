3303a524e674e1cdcd219cb18de0b68396685c6b
Amos <amos7perez@gmail.com>
2025-09-18 12:39:21 -0400

    Add auth check and add session variable user_id to inData payload

--- application/api/contacts/read/search.php
+++ application/api/contacts/read/search.php
@@ -6,6 +6,14 @@
 
     $inData = getRequestInfo();
 
+    session_start();
+
+    if (!isset($_SESSION['user_id'])) {
+        http_response_code(401);
+        returnWithError("Not authenticated");
+        exit;
+    }
+
     $pdo = db();
 
     if (!isset($inData["page_index"]) || !isset($inData["contacts_per_page"]) || !isset($inData["search_term"]) || !isset($inData["user_id"])) {
